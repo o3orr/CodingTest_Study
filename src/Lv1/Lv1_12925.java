1c7d195fb59b82ed1d67a90ba6585e568a150213
choiyoungseo <chlchl0506@naver.com>
2024-04-14 19:06:07 +0900

    commit

---
+++ src/Lv1/Lv1_12925.java
@@ -0,0 +1,15 @@
+package Lv1;
+
+/*
+   문자열을 정수로 바꾸기
+   https://school.programmers.co.kr/learn/courses/30/lessons/12925
+ */
+
+public class Lv1_12925 {
+    class Solution {
+        public int solution(String s) {
+            int answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+}
